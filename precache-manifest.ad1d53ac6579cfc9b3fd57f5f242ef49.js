911613e70faf5ebc7c286da0c6782c14d4c9bec4
Siddharaj Suryavanshi <siddharaj@highway.nsyst.com>
2018-11-16 15:54:18 +0530

    Updates

---
+++ precache-manifest.ad1d53ac6579cfc9b3fd57f5f242ef49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d1ea7a84d35c47b27ea",
+    "url": "/react_todo_app_master/static/js/runtime~main.7d1ea7a8.js"
+  },
+  {
+    "revision": "61444174410c3981dd25",
+    "url": "/react_todo_app_master/static/js/main.61444174.chunk.js"
+  },
+  {
+    "revision": "62d8ccf9ea270606c305",
+    "url": "/react_todo_app_master/static/js/1.62d8ccf9.chunk.js"
+  },
+  {
+    "revision": "61444174410c3981dd25",
+    "url": "/react_todo_app_master/static/css/main.4ae71030.chunk.css"
+  },
+  {
+    "revision": "4a6f2ef036e5fd0b8ae48921ed176002",
+    "url": "/react_todo_app_master/index.html"
+  }
+];
